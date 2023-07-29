e9c9bac0d62cea7469269a251255a5e551ecf40c
Rohulla123 <rohullah.asify@gmail.com>
2023-07-29 10:33:57 -0400

    impemented

--- src/GitProject1/DemoDay2.java
+++ src/GitProject1/DemoDay2.java
@@ -4,5 +4,13 @@ public class DemoDay2 {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
+		
+		
 	}
+		
+		public static void add(int a, int b) {
+			
+			System.out.println(a+b);
+		}
+	
 }
