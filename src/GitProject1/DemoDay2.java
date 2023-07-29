1590f832dc2b3b711048ed9e33d423ba24e3b1a1
Rohulla123 <94883386+Rohulla123@users.noreply.github.com>
2023-07-29 10:49:36 -0400

    Update DemoDay2.java

--- src/GitProject1/DemoDay2.java
+++ src/GitProject1/DemoDay2.java
@@ -8,9 +8,9 @@ public class DemoDay2 {
 		
 	}
 		
-		public static void add(int a, int b) {
+		public static void add(int a, int b, int c) {
 			
-			System.out.println(a+b);
+			System.out.println(a+b +c);
 		}
 	
 }
